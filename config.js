07b759d20566638370d1c809d930bcae920a4268
carlwittmann <113217094+carlwittmann@users.noreply.github.com>
2023-04-24 20:13:17 -0700

    Maopbox style edits and camera control edits

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 var config = {
-    style: 'mapbox://styles/mapbox/streets-v11',
-    accessToken: 'pk.eyJ1IjoiaW9yb21pZWgiLCJhIjoiY2xnMm5ha3d0MDB3ajNscG5sZXk3Ym8zbyJ9.KDoXNMToFN3DyVwSTEwvsQ',
-    showMarkers: true,
+    style: 'mapbox://styles/adversarial-geography/clgvksdqt002x01pof9qqatn1',
+    accessToken: 'pk.eyJ1IjoiYWR2ZXJzYXJpYWwtZ2VvZ3JhcGh5IiwiYSI6ImNsZjBvN3FqbjAwZDkzcmw0MGsxNDJ3MDIifQ.aVzxEUcFBUiojBtwfqYe6w',
+    showMarkers: false,
     markerColor: '#3FB1CE',
     //projection: 'equirectangular',
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
     inset: true,
     theme: 'dark',
-    use3dTerrain: true, //set true for enabling 3D maps.
+    use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'The Title Text of this Story',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
+    title: 'SOMETHING SNAZZY ABOUT WATER',
+    subtitle: 'SOMETHING DESCRIPTIVE ABOUT THE PROJECT',
+    byline: 'By ______',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
+            title: 'ESTABLISHING SHOT OF NORTH AMERICA',
             image: './path/to/image/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: 'Our introduction goes blah blah blahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: 0
+                center: [-81.141, 28.356],
+                zoom: 3.4,
+                pitch: 20,
+                bearing: -45
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -49,12 +49,12 @@ var config = {
             id: 'second-identifier',
             alignment: 'right',
             hidden: false,
-            title: 'Second Title',
+            title: 'APPROACHING VANCOUVER',
             image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'Blah blah blahhhhhhhh',
             location: {
-                center: [-77.020636, 38.886900],
-                zoom: 8.5,
+                center: [-122.875, 49.379],
+                zoom: 4,
                 pitch: 60,
                 bearing: -43.2,
                 // flyTo additional controls-
@@ -74,13 +74,13 @@ var config = {
             id: 'third-identifier',
             alignment: 'left',
             hidden: false,
-            title: 'Third Title',
+            title: 'You get it by now',
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [6.15116, 46.20595],
-                zoom: 12.52,
-                pitch: 8.01,
+                center: [-122.875, 49.379],
+                zoom: 7,
+                pitch: 0,
                 bearing: 0.00
             },
             mapAnimation: 'flyTo',
@@ -97,13 +97,13 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
-                center: [-58.54195, -34.71600],
-                zoom: 4,
-                pitch: 0,
+                center: [-122.875, 49.379],
+                zoom: 10,
+                pitch: 60,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            rotateAnimation: true,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
